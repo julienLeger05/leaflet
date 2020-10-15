90a29bab1b884e53da8d874206832fe4d8958cdc
moi <julien@macbook-pro-de-julien.home>
2020-10-15 20:28:10 +0200

    c

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
 let villes = {
-    "Gap": [44.566667, 6.083333],
-    "Nice": [43.696036, 7.265592],
-    "Paris": [48.856614, 2.352221]
-}
-let nice = document.querySelector('.nice')
-let paris = document.querySelector('.paris')
-let gap = document.querySelector('.gap')
-document.querySelector('.gap').addEventListener('click', () => {
-    gap.value = villes.Gap;
-    nice.value = "";
-    paris.value = ""
-})
+  Gap: [44.566667, 6.083333],
+  Nice: [43.696036, 7.265592],
+  Paris: [48.856614, 2.352221],
+};
+let c = document.querySelector(".c");
+// Quand une nouvelle <option> est selectionnée
+console.log(c.options[c.selectedIndex].value);
+let coo = c.options[c.selectedIndex].value;
+/* marker.addTo(carte) */
+var carte = L.map("maCarte").setView(
+  JSON.parse(coo),
+  10
+); /* On aurait aussi pu écrirelet carte = L.map('maCarte').setView([46.3630104, 2.9846608],5);*/
+L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(carte);
+/* Ò */
+c.addEventListener("change", function () {
+  let coo = c.options[c.selectedIndex].value;
+  console.log(coo);
 
-document.querySelector('.nice').addEventListener('click', () => {
-        gap.value = "";
-        nice.value = villes.Nice;
-        paris.value = ""
-    })
-    /* marker.addTo(carte) */
+  var marker = L.marker(JSON.parse(coo)).addTo(carte);
 
-document.querySelector('.paris').addEventListener('click', () => {
-        gap.value = "";
-        nice.value = "";
-        paris.value = villes.Paris
-
-    })
-    /* marker.addTo(carte) */
-let carte = L.map('maCarte', { center: [46.3630104, 2.9846608], zoom: 5 }); /* On aurait aussi pu écrirelet carte = L.map('maCarte').setView([46.3630104, 2.9846608],5);*/
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', { attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' }).addTo(carte);
+  carte.setView(JSON.parse(coo), 10);
+});
